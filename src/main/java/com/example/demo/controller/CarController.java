7bc4bab9476cd4502982a861e538f2a18a594f76
tino <tino@tog.co.id>
2024-01-15 20:20:23 +0700

    first commit

---
+++ src/main/java/com/example/demo/controller/CarController.java
@@ -0,0 +1,40 @@
+package com.example.demo.controller;
+
+import com.example.demo.entity.Car;
+import com.example.demo.repository.CarRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/api/v1")
+public class CarController {
+
+    @Autowired
+    CarRepository carRepository;
+
+    @PostMapping({"/car", "/car/"})
+    public ResponseEntity<Map<String, Object>> createCar(@RequestBody Car car) {
+        Map<String, Object> res = new HashMap<>();
+
+        try{
+            carRepository.save(car);
+        }catch(Exception e) {
+            res.put("is_success", false);
+            res.put("message", "error "+e.getMessage());
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(res);
+        }
+
+        res.put("is_success", true);
+        res.put("message", "success");
+        res.put("car", car);
+        return ResponseEntity.ok(res);
+    }
+}
---
+++ src/main/java/com/example/demo/entity/Car.java
@@ -0,0 +1,69 @@
+package com.example.demo.entity;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "car")
+public class Car {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String id;
+
+    @Column(name = "brand")
+    @JsonProperty("brand")
+    private String branch;
+
+    @Column(name = "series")
+    @JsonProperty("series")
+    private String series;
+
+    @Column(name = "top_speed_kmph")
+    @JsonProperty("top_speed_kmph")
+    private Integer topSpeedKmph;
+
+    @Column(name = "country")
+    @JsonProperty("country")
+    private String country;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getBranch() {
+        return branch;
+    }
+
+    public void setBranch(String branch) {
+        this.branch = branch;
+    }
+
+    public String getSeries() {
+        return series;
+    }
+
+    public void setSeries(String series) {
+        this.series = series;
+    }
+
+    public Integer getTopSpeedKmph() {
+        return topSpeedKmph;
+    }
+
+    public void setTopSpeedKmph(Integer topSpeedKmph) {
+        this.topSpeedKmph = topSpeedKmph;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+}

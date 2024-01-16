68dc79a0810883ebb2eef20c8cf97c9e34d76182
fikytaufik <fikytaufik8@gmail.com>
2024-01-16 15:54:42 +0700

    add: list Car and Update Car

--- src/main/java/com/example/demo/controller/CarController.java
+++ src/main/java/com/example/demo/controller/CarController.java
@@ -5,12 +5,10 @@ import com.example.demo.repository.CarRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 @RestController
@@ -19,6 +17,11 @@ public class CarController {
 
     @Autowired
     CarRepository carRepository;
+
+    @GetMapping({"/cars","cars"})
+    public List<Car> indexAction(){
+        return carRepository.findAll();
+    }
 
     @PostMapping({"/cars", "/cars/"})
     public ResponseEntity<Map<String, Object>> createCar(@RequestBody Car car) {
@@ -35,6 +38,28 @@ public class CarController {
         res.put("is_success", true);
         res.put("message", "success");
         res.put("car", car);
+        return ResponseEntity.ok(res);
+    }
+
+    @PutMapping({"/cars/{id}", "/cars/{id}"})
+    public ResponseEntity<Map<String, Object>> updateCar(@PathVariable("id") Car oldCar, @RequestBody Car newCar){
+        Map<String, Object> res = new HashMap<>();
+
+        try{
+            oldCar.setBranch(newCar.getBranch());
+            oldCar.setSeries(newCar.getSeries());
+            oldCar.setTopSpeedKmph(newCar.getTopSpeedKmph());
+            oldCar.setCountry(newCar.getCountry());
+            carRepository.save(newCar);
+        }catch(Exception e) {
+            res.put("is_success", false);
+            res.put("message", "error "+e.getMessage());
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(res);
+        }
+
+        res.put("is_success", true);
+        res.put("message", "success");
+        res.put("car", oldCar);
         return ResponseEntity.ok(res);
     }
 }

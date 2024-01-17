f347d0fd9ef5122c89f4cc8ac54fa5d1ebbed1a2
fikytaufik <fikytaufik8@gmail.com>
2024-01-17 17:22:25 +0700

    fix: parameter for swagger

--- src/main/java/com/example/demo/controller/CarController.java
+++ src/main/java/com/example/demo/controller/CarController.java
@@ -50,24 +50,28 @@ public class CarController {
     }
 
     @PutMapping({"/cars/{id}", "/cars/{id}"})
-    public ResponseEntity<Map<String, Object>> updateCar(@PathVariable("id") Car oldCar, @RequestBody Car newCar){
+    public ResponseEntity<Map<String, Object>> updateCar(@PathVariable("id") @Parameter(name = "id", description = "Car id", example = "1", required = true) String id, @RequestBody Car newCar){
         Map<String, Object> res = new HashMap<>();
 
         try{
-            oldCar.setBranch(newCar.getBranch());
-            oldCar.setSeries(newCar.getSeries());
-            oldCar.setTopSpeedKmph(newCar.getTopSpeedKmph());
-            oldCar.setCountry(newCar.getCountry());
-            carRepository.save(newCar);
+            Optional<Car> oldCar = carRepository.findById(id);
+            Car carUpdate = oldCar.get();
+            carUpdate.setName(newCar.getName());
+            carUpdate.setBranch(newCar.getBranch());
+            carUpdate.setSeries(newCar.getSeries());
+            carUpdate.setTopSpeedKmph(newCar.getTopSpeedKmph());
+            carUpdate.setCountry(newCar.getCountry());
+            carRepository.save(carUpdate);
+
+            res.put("is_success", true);
+            res.put("message", "success");
+            res.put("car", oldCar);
         }catch(Exception e) {
             res.put("is_success", false);
             res.put("message", "error "+e.getMessage());
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(res);
         }
 
-        res.put("is_success", true);
-        res.put("message", "success");
-        res.put("car", oldCar);
         return ResponseEntity.ok(res);
     }
 
